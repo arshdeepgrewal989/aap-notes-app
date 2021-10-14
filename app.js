a89cf0075b1a4faf609e3170457841bac0576cc6
Arshdeep <arshdeepgrewal989@gmail.com>
2021-10-14 16:13:18 -0400

    init

---
+++ app.js
@@ -0,0 +1,120 @@
+
+const chalk = require('chalk');
+const { argv } = require('yargs');
+const yargs= require('yargs');
+const { listNotes } = require('./notes.js');
+const notes = require('./notes.js');
+
+const getNotes= require('./notes.js')
+
+const command = process.argv[2]
+
+//customizing version yargs
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'The contents of the note',
+            type: "string",
+            demandOption: true
+        }
+
+
+    },
+    // argv = argument vector - the arguments that the user passes thru program
+    handler: function(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// remove note command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Title of Note to be removed',
+            type: "string",
+            demandOption: true,
+        }
+    },
+    // rem to name the argv argument there
+    handler(argv) {
+        notes.removeNote(argv.title)
+        
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing all the notes',
+    handler() {
+        notes.listNotes()
+        
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+       title: {
+           describe: 'Note title',
+           demandOption: true,
+           type: 'string'
+       } 
+    }, 
+    handler(){
+        console.log('The note reads...')
+        notes.printNote(argv.title)
+    }
+})
+
+
+// output
+//console.log(yargs.argv)
+
+// output with yargs
+yargs.parse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this gets input from command line. [] can be used to select what to output
+// console.log(process.argv[2])
+
+
+
+
+
+
+// old stuff
+//const validator = require('validator')
+//const getNotes = require('./notes.js')
+
+//const msg = getNotes()
+
+//console.log(msg)
+
+
+//console.log(validator.isURL('https://arshdeep.com'))"
---
+++ notes.js
@@ -0,0 +1,109 @@
+const chalk = require('chalk');
+const fs = require('fs');
+const { argv } = require('process');
+
+// => replaces function... rem: function came first, but => comes after ()
+const addNote = (title, body) => {
+
+    const notes = loadNotes()
+
+    // to check if any new notes have conflicting names
+    // const duplicateNotes = notes.filter(function(note){
+    //     return note.title == title
+    // })
+
+    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    debugger
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.bgGreenBright.inverse('Note successfully added.'))
+    } else {
+        console.log(chalk.bgRedBright('Err: Conflicting name with a previous note.'))
+    }
+
+    
+}
+
+// Function to remove note: Arg- Title
+const removeNote= (title) => {
+
+    const notes = loadNotes()
+
+    // const notesToKeep= notes.filter(function(note){
+    //     return note.title != title
+    // })
+
+    const notesToKeep= notes.filter((note) => note.title !== title)
+    
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.bgGreenBright('Note Successfully removed'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.bgRedBright('Error'))
+    }
+
+}
+
+const listNotes = () => {
+    
+    const notes= loadNotes()
+
+    console.log(chalk.inverse('Your notes:'))
+    
+    notes.forEach((note) => {
+        console.log(chalk.white(note.title))
+    })
+}
+
+const printNote = (title) => {
+
+    const notes = loadNotes()
+
+    //const noteRequested= notes.filter((title) => note.title === title)
+
+    const noteRequested = notes.find((noteRequested) => noteRequested.title === title)
+
+    if (noteRequested){
+        console.log(chalk.inverse(noteRequested.title))
+        console.log(chalk.white(noteRequested.body))
+    } else {
+        console.log(chalk.bgRedBright('Not found'))
+    }
+
+}
+
+// aux fn to save note
+const saveNotes= (notes) => {
+    const dataJSON= JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+// aux fn to load up notes
+const loadNotes = () => {
+
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+
+// the .export obj allows us to export and make use of the fn's from this file in other files
+// having {} there allows us to make the export into an obj and export multiple fn's
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    printNote: printNote  
+}
